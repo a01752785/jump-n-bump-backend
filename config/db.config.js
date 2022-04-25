1a5d5e85e356e46ef58195d7205a8e955238b815
a01752785 <a01752785@itesm.mx>
2022-04-25 14:38:34 -0500

    Change gitignore

---
+++ config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "jump-n-bump-server.database.windows.net",
+    USER: "a01752785",
+    PASSWORD: "Y6K7h%4FTKpF",
+    DB: "jump-n-bump-database", 
+    dialect: "mssql"
+};
