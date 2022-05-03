9fe05b4718ea28f144abbbd202449431dc273cfb
a01752785 <a01752785@itesm.mx>
2022-05-02 22:35:32 -0500

    Add Swagger register documentation

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./models");
 
+const path = require("path");
+
+// Swagger documentation
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Jump N Bump API",
+            version: "1.0.0"
+        }
+    },
+    apis: [`${path.join(__dirname, "./routes/*")}`]
+};
 // Set up express app
 const app = express();
 
@@ -22,9 +37,11 @@ app.use(function(err, req, res, next){
         var errorStr = err.name;
         if (err.name == "SequelizeForeignKeyConstraintError")
             errorStr = errorStr + ":" + err.index;
-        res.status(422).send(err.name);
+        res.status(422).send(err);
     }
 });
+
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 
 // Listen for requests
 app.listen(process.env.port || 4000, function() {
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -5,6 +5,73 @@ const PlayerCollectibleController = require("../controllers/player.collectible.c
 var router = express.Router();
 
 // Create a new player
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Player:
+ *          type: object
+ *          properties:
+ *              alias:
+ *                  type: string
+ *                  description: Alias of the player
+ *              fechaNacimiento:
+ *                  type: string
+ *                  description: Date of birth of the player in AAAA/MM/DD
+ *              nombre:
+ *                  type: string
+ *                  description: First name of the player
+ *              apellido:
+ *                  type: string
+ *                  description: Last name of the player
+ *              genero:
+ *                  type: string
+ *                  description: Gender or the player (Male/Female)
+ *              nacionalidad:
+ *                  type: string
+ *                  description: Nationality of the player
+ *              password:
+ *                  type: string
+ *                  description: Password of the player
+ *          required:
+ *              - alias
+ *              - fechaNacimiento
+ *              - nombre
+ *              - apellido
+ *              - genero
+ *              - nacionalidad
+ *              - password
+ *          example:
+ *              alias: jumpnbump
+ *              fechaNacimiento: 2002/05/23
+ *              nombre: Andrea
+ *              apellido: Velazquez
+ *              genero: Female
+ *              nacionalidad: Peru
+ *              password: pass123
+ */
+
+/**
+ * @swagger
+ * /api/register:
+ *  post:
+ *      summary: Create a new player
+ *      tags: [Player]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/Player'
+ *      responses:
+ *          200:
+ *              description: OK
+ *          422:
+ *              description: Error
+ */
+
 router.post("/register", function(req, res, next) {
     const jugador = {
         alias: req.body.alias,
